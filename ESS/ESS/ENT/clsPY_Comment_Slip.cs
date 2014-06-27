78b61199374fe17b8afa3132160e7dd2920bcae7
Luckana Sutthidee <nan_luckana@hotmail.com>
2014-06-27 10:49:54 +0700

    PY

---
+++ ESS/ESS/ENT/clsPY_Comment_Slip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESS.ENT
+{
+    public class clsPY_Comment_Slip
+    {
+        public String TITLE
+        {
+            get;
+            set;
+        }
+        public String COMMENT_SLIP_DESC
+        {
+            get;
+            set;
+        }
+        public DateTime START_EFF_DT
+        {
+            get;
+            set;
+        }
+        public DateTime END_EFF_DT
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ESS/ESS/UI/PY/CommentSlip.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ESS.UI.PY
+{
+    public partial class CommentSlip : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+            cldStart.Visible = false;
+            cldEnd.Visible = false;
+            }
+        }
+
+        protected void imgbtnStartdate_Click(object sender, ImageClickEventArgs e)
+        {
+            if (cldStart.Visible == false)
+            {
+                cldStart.Visible = true;
+            }
+            else
+            {
+                cldStart.Visible = false;
+            }
+
+        }
+        protected void cldStart_SelectionChanged(object sender, EventArgs e)
+        {
+            txtStartDate.Text = cldStart.SelectedDate.ToShortDateString();
+        }
+
+        protected void imgbtnEnddate_Click(object sender, ImageClickEventArgs e)
+        {
+            if (cldEnd.Visible == false)
+            {
+                cldEnd.Visible = true;
+            }
+            else
+            {
+                cldEnd.Visible = false;
+            }
+        }
+
+        protected void cldEnd_SelectionChanged(object sender, EventArgs e)
+        {
+            txtEndDate.Text = cldEnd.SelectedDate.ToShortDateString();
+        }
+
+        protected void btnNew_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+       
+    }
+}
